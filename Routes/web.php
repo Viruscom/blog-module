d1952ee326ae22be9a4be032c73947cbc9d770fd
Hristiyan Chobanov <hristiqn.chobanov@gmail.com>
2022-02-03 14:32:08 +0200

    123

--- Routes/web.php
+++ Routes/web.php
@@ -20,12 +20,14 @@ Route::prefix('blog')->group(function() {
 
 /* Admin */
 Route::group(['prefix' => 'admin'], static function () {
-    Route::get('/', [BlogController::class, 'index'])->name('blog');
-    Route::get('/create', [BlogController::class, 'create'])->name('blog.create');
-    Route::post('/store', [BlogController::class, 'store'])->name('blog.store');
-    Route::get('/{id}/edit', [BlogController::class, 'edit'])->name('blog.edit');
-    Route::post('/{id}/update', [BlogController::class, 'update'])->name('blog.update');
-    Route::delete('/{id}/delete', [BlogController::class, 'delete'])->name('blog.delete');
-    Route::get('/{id}/show', [BlogController::class, 'show'])->name('blog.show');
-    Route::get('/active/{id}/{active}', [BlogController::class, 'changeActiveStatus'])->name('blog.changeStatus');
+    Route::group(['prefix' => 'blog'], static function () {
+        Route::get('/', [BlogController::class, 'index'])->name('blog');
+        Route::get('/create', [BlogController::class, 'create'])->name('blog.create');
+        Route::post('/store', [BlogController::class, 'store'])->name('blog.store');
+        Route::get('/{id}/edit', [BlogController::class, 'edit'])->name('blog.edit');
+        Route::post('/{id}/update', [BlogController::class, 'update'])->name('blog.update');
+        Route::delete('/{id}/delete', [BlogController::class, 'delete'])->name('blog.delete');
+        Route::get('/{id}/show', [BlogController::class, 'show'])->name('blog.show');
+        Route::get('/active/{id}/{active}', [BlogController::class, 'changeActiveStatus'])->name('blog.changeStatus');
+    });
 });
